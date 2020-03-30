09aba2985aec0b09c51e8bfc77eff4fdc09fca79
venkat <penubothuvenkannachowdary@gmail.com>
2020-03-30 22:01:53 +0530

    added game

---
+++ content.js
@@ -0,0 +1,184 @@
+const blockedpages = ["xvideos.com","xhamster.com","pornhub.com","xnxx.com","redtube.com","youporn.com","tube8.com","youjizz.com","hardsextube.com","beeg.com","motherless.com","drtuber.com","nuvid.com","pornerbros.com","spankwire.com","keezmovies.com","sunporno.com","porn.com","4tube.com","alphaporno.com","xtube.com","pornoxo.com","yobt.com","tnaflix.com","pornsharia.com","brazzers.com","extremetube.com","slutload.com","fapdu.com","empflix.com","alotporn.com","vid2c.com","Shufuni.com","cliphunter.com","xxxbunker.com","madthumbs.com","deviantclip.com","twilightsex.com","pornhost.com","fux.com","jizzhut.com","spankbang.com","eporner.com","orgasm.com","yuvutu.com","kporno.com","definebabe.com","secret.shooshtime.com","mofosex.com","hotgoo.com","submityourflicks.com","xxx.com","bigtits.com","media.xxxaporn.com","bonertube.com","userporn.com","jizzonline.com","pornotube.com","fookgle.com","free18.net","tub99.com","nonktube.com","mastishare.com","tjoob.com","rude.com","bustnow.com","pornrabbit.com","pornative.com","sluttyred.com","boysfood.com","moviefap.com","lubetube.com","submityourtapes.com","megafilex.com","hdpornstar.com","al4a.com","stileproject.com","xogogo.com","filthyrx.com","jizzbo.com","5ilthy.com","91porn.com","lesbianpornvideos.com","eroxia.com","iyottube.com","yourfreeporn.us","sexoasis.com","fucktube.com","pornomovies.com","clearclips.com","moviesand.com","tubixe.com","pornjog.com","sextv1.pl","desihoes.com","pornupload.com","kosimak.com","videocasalinghi.com","lubeyourtube.com","freudbox.com","moviesguy.com","motherofporn.com","141tube.com","my18tube.com","bigupload.comxvds.com","fastjizz.com","tubeland.com","ultimatedesi.net","teenporntube.com","tubave.com","afunnysite.com","sexe911.com","megaporn.com","porntitan.com","pornheed.com","youhot.gr","videolovesyou.com","onlymovies.com","hdporn.net","adultvideodump.com","suzisporn.com","xfilmes.tv","pornwall.com","silverdaddiestube.com","sextube.sweetcollegegirls.com","ipadporn.com","youporns.org","movietitan.com","yaptube.com","jugy.com","chumleaf.com","panicporn.com","milfporntube.com","timtube.com","wetpussy.com","whoreslag.com","xfapzap.com","xvideohost.com","tuberip.com","dirtydirtyangels.com","bigerotica.com","pk5.net","theamateurzone.info","triniporn.org","youbunny.com","isharemybitch.com","morningstarclub.com","sexkate.com","kuntfutube.com","porncor.com","thegootube.com","tubeguild.com","fuckuh.com","tube.smoder.com","zuzandra.com","nextdoordolls.com","myjizztube.com","homesexdaily.com","thetend.com","yourpornjizz.com","tgirls.com","pornwaiter.com","pornhub.pl","nurglestube.com","brazzershdtube.com","upthevideo.com","sexzworld.com","cuntest.com","ahtube.com","free2peek.com","freeamatube.com","thexxxtube.net","yazum.com","tubesexes.com","pornload.com","vankoi.com","dailee.com","ejason21.com","openpunani.com","porntubexl.nl","scafy.com","bangbull.com","vidxnet.com","yteenporn.com","tubethumbs.com","faptv.com","nasty8.com","maxjizztube.com","pornunder.com","24h-porn.net","xclip.tv","jerkersworld.com","desibomma.com","jizzbox.com","theyxxx.com","bonkwire.com","PornTelecast.com","pornsitechoice.com","yporn.tv","girlsongirlstube.com","famouspornstarstube.com","sexfans.org","youpornxl.com","rudeshare.com","efuckt.com","koostube.com","amateursex.com","moviegator.com","cobramovies.com","cantoot.com","yourhottube.com","teentube18.com","youxclip.com","flicklife.com","nastyrat.tv","freepornfox.com","freeadultwatch.com","fucked.tv","sextube.si","pornrater.com","wheresmygf.com","xfanny.com","pornorake.com","untouched.tv","guppyx.com","mylivesex.tv","pervaliscious.com","sex2ube.com","suckjerkcock.com","netporn.nl","exgfvid.com","koalaporn.com","bbhgvidz.com","evilhub.com","celebritytubester.com","pornfish.com","jrkn.com","bootyclips.com","tubeguide.info","realhomemadetube.com","tokenxxxporn.com","pornvideoflix.com","sinfultube.net","pornler.com","sharexvideo.com","69youPorn.com","submitmyvideo.com","kastit.com","pornini.com","hd4sex.com","laftube.com","mosestube.com","dutchxtube.com","porncastle.net","tubedatbooty.com","pornvie.com","pornopantry.com","springbreaktubegirls.com","tube4u.net","nsfwftw.com","pornozabava.com","celebritynudez.com","teeztube.com","collegefucktube.com","adultvideomate.com","porntubemoviez.com","bigjuggs.com","hornypickle.com","mypornow.com","pushingpink.com","xxxshare.ru","nuuporn.com","melontube.com","myamateurporntube.com","soyouthinkyourapornstar.com","porntubestreet.com","pornogoddess.com","cumsnroses.com","porntubeclipz.com","lcgirls.com","housewifes.com","cactarse.com","cumfox.com","tube17.com","teenbrosia.com","lesbiantubemovies.com","xxxset.com","gagahub.com","jugland.com","porntubesurf.com","freakybuddy.com","sexdraw.com","sexovirtual.com","pornsmack.com","gratisvideokijken.nl","eroticadulttube.com","bharatporn.com","fmeporn.com","darkpost.com","sexporndump.com","sexandporn.org","jezzytube.com","justpornclip.com","xxxpornow.com","inseks.com","freeporn777.com","porndisk.com","adultfunnow.com","youporn.us.com","babecumtv.com","girlskissinggirlsvideos.com","specialtytubeporn.com","teentube.be","free‐celebrity‐tube.com","public‐sluts.net"];
+
+var url = window.location.href;
+
+blockedpages.forEach(site => {
+    var regex = new RegExp(site.trim(), "i");
+	var pornfinder = regex.test(url);
+	if(pornfinder){
+		// chrome.runtime.sendMessage({visited: true, url: url});
+
+		$(document).ready(function(){
+		    document.body.innerHTML = '';
+			$('body').append(`
+			 <canvas id="canvasdemo" width="460" height="340"></canvas>
+			`);
+			var canvas= document.getElementById("canvasdemo");
+			var ctx = canvas.getContext("2d");
+			var ballRadius = 10;
+            var x = canvas.width/2;
+            var y = canvas.height-30;
+            var dx = 2;
+            var dy = -2;
+            var paddleHeight = 10;
+            var paddleWidth = 75;
+            var paddleX = (canvas.width-paddleWidth)/2;
+            var rightPressed = false;
+            var leftPressed = false;
+            var brickRowCount = 4;
+            var brickColumnCount = 3;
+            var brickWidth = 75;
+            var brickHeight = 15;
+            var brickPadding = 30;
+            var brickOffsetTop = 30;
+            var brickOffsetLeft = 10;
+            var score = 0;
+            var lives = 3;
+			
+			var bricks = [];
+			for(var c=0; c<brickColumnCount; c++) {
+				bricks[c] = [];
+				for(var r=0; r<brickRowCount; r++) {
+				  bricks[c][r] = { x: 0, y: 0, status: 1 };
+				}
+			  }
+			  document.addEventListener("keydown", keyDownHandler, false);
+			  document.addEventListener("keyup", keyUpHandler, false);
+			  document.addEventListener("mousemove", mouseMoveHandler, false);
+			  
+			  function keyDownHandler(e) {
+				  if(e.key == "Right" || e.key == "ArrowRight") {
+					  rightPressed = true;
+				  }
+				  else if(e.key == "Left" || e.key == "ArrowLeft") {
+					  leftPressed = true;
+				  }
+			  }
+			  
+			  function keyUpHandler(e) {
+				  if(e.key == "Right" || e.key == "ArrowRight") {
+					  rightPressed = false;
+				  }
+				  else if(e.key == "Left" || e.key == "ArrowLeft") {
+					  leftPressed = false;
+				  }
+			  }
+			  
+			  function mouseMoveHandler(e) {
+				var relativeX = e.clientX - canvas.offsetLeft;
+				if(relativeX > 0 && relativeX < canvas.width) {
+				  paddleX = relativeX - paddleWidth/2;
+				}
+			  }
+			  function collisionDetection() {
+				for(var c=0; c<brickColumnCount; c++) {
+				  for(var r=0; r<brickRowCount; r++) {
+					var b = bricks[c][r];
+					if(b.status == 1) {
+					  if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+						dy = -dy;
+						b.status = 0;
+						score++;
+						if(score == brickRowCount*brickColumnCount) {
+						  alert("Hola,You Won!");
+						  document.location.reload();
+						}
+					  }
+					}
+				  }
+				}
+			  }
+			  
+			  function drawBall() {
+				ctx.beginPath();
+				ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			  }
+			  function drawPaddle() {
+				ctx.beginPath();
+				ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			  }
+			  function drawBricks() {
+				for(var c=0; c<brickColumnCount; c++) {
+				  for(var r=0; r<brickRowCount; r++) {
+					if(bricks[c][r].status == 1) {
+					  var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+					  var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+					  bricks[c][r].x = brickX;
+					  bricks[c][r].y = brickY;
+					  ctx.beginPath();
+					  ctx.rect(brickX, brickY, brickWidth, brickHeight);
+					  ctx.fillStyle = "#0095DD";
+					  ctx.fill();
+					  ctx.closePath();
+					}
+				  }
+				}
+			  }
+			  function drawScore() {
+				ctx.font = "16px Arial";
+				ctx.fillStyle = "#0095DD";
+				ctx.fillText("Score: "+score, 8, 20);
+			  }
+			  function drawLives() {
+				ctx.font = "16px Arial";
+				ctx.fillStyle = "#0095DD";
+				ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+			  }
+			  
+			  function draw() {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				drawBricks();
+				drawBall();
+				drawPaddle();
+				drawScore();
+				drawLives();
+				collisionDetection();
+			  
+				if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+				  dx = -dx;
+				}
+				if(y + dy < ballRadius) {
+				  dy = -dy;
+				}
+				else if(y + dy > canvas.height-ballRadius) {
+				  if(x > paddleX && x < paddleX + paddleWidth) {
+					dy = -dy;
+				  }
+				  else {
+					lives--;
+					if(!lives) {
+					  alert("GAME OVER");
+					  document.location.reload();
+					}
+					else {
+					  x = canvas.width/2;
+					  y = canvas.height-30;
+					  dx = 3;
+					  dy = -3;
+					  paddleX = (canvas.width-paddleWidth)/2;
+					}
+				  }
+				}
+			  
+				if(rightPressed && paddleX < canvas.width-paddleWidth) {
+				  paddleX += 7;
+				}
+				else if(leftPressed && paddleX > 0) {
+				  paddleX -= 7;
+				}
+			  
+				x += dx;
+				y += dy;
+				requestAnimationFrame(draw);
+			  }
+			  
+			  draw();
+		  });
+	}
+});
